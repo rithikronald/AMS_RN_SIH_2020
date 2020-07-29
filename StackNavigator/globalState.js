6b59ff5a5f979ca3b2ff609105dc86f6386bc9c5
vkyscen <vickyeditz@gmail.com>
2020-07-29 18:09:42 +0530

    postreport api consumed

--- StackNavigator/globalState.js
+++ StackNavigator/globalState.js
@@ -1,11 +1,25 @@
 import React, { useState, createContext } from "react";
+import Axios from "axios";
+const url = require("../assets/constants").url;
 
 export const GlobalContext = createContext();
 
 export const GlobalState = (props) => {
   const [finalReport, setFinalReport] = useState([]);
   const [finalReview, setFinalReview] = useState([]);
-
+  function postreport(finalReport, finalReview, visitId) {
+    axios
+      .post(url + "postreport/" + visitId, {
+        reportData: finalReport,
+        remarks: finalReview,
+      })
+      .then((d) => {
+        console.log(d);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <GlobalContext.Provider
       value={{ finalReport, setFinalReport, finalReview, setFinalReview }}
